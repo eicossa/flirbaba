0c3c93b0aafe45099032c4599cf0eca8956cb596
Arjun J Rao <rectangle.king@gmail.com>
2018-06-21 12:08:28 +0530

    calcSmax, calcSmin and calcSdelta all updated

--- src/calcStuff.cpp
+++ src/calcStuff.cpp
@@ -1,5 +1,8 @@
+#include <iostream>
+#include <math>
 #include "../inc/readTags.hpp"
 
+using namespace std;
 
 double calcRAWmax()
 {
@@ -66,50 +69,122 @@ double calcRAWminobj()
   return rawminobj;
 }
 
-void calculateMaxMinTemperatures()
+double calcTmin()
 {
-  double t21 = (planckr2 * (rawminobj + plancko) + planckf);
-  cout << " Tmin t21 : " << t21 << endl;
-  double t22 = (planckr1/t21) - 273.15;
-  cout << " Tmin t22 : " << t22 << endl;
-  double t23 = log(-t22);
-  cout << " Tmin t23 : " << t23 << endl;
+  double planckr1, planckr2;
+  double planckb, planckf, plancko;
+  double rawminobj;
+  double tmin;
+  double t21, t22, t23;
+
+  planckr1  = getPlanckR1();
+  planckr2  = getPlanckR2();
+  planckb   = getPlanckB();
+  planckf   = getPlanckF();
+  plancko   = getPlanckO();
+  rawminobj = calcRAWminobj();
+  
+  t21 = (planckr2 * (rawminobj + plancko) + planckf);
+  //cout << " Tmin t21 : " << t21 << endl;
+  t22 = (planckr1/t21) - 273.15;
+  //cout << " Tmin t22 : " << t22 << endl;
+  t23 = log(-t22);
+  //cout << " Tmin t23 : " << t23 << endl;
   
   tmin = planckb / t23;
-  cout << " Tmin     : " << tmin << endl;
+  //cout << " Tmin     : " << tmin << endl;
+  return tmin;
+}
 
-  double t31 = (planckr2 * (rawmaxobj + plancko) + planckf);
-  cout << " Tmax t31 : " << t31 << endl;
-  double t32 = (planckr1/t31) - 273.15;
-  cout << " Tmax t32 : " << t32 << endl;
-  double t33 = log(-t32);
-  cout << " Tmax t33 : " << t33 << endl;
+double calcTmax()
+{
+  double planckr1, planckr2;
+  double planckb, planckf, plancko;
+  double rawmaxobj;
+  double tmax;
+  double t31, t32, t33;
+
+  planckr1  = getPlanckR1();
+  planckr2  = getPlanckR2();
+  planckb   = getPlanckB();
+  planckf   = getPlanckF();
+  plancko   = getPlanckO();
+  rawmaxobj = calcRAWmaxobj();
+
+  t31 = (planckr2 * (rawmaxobj + plancko) + planckf);
+  //cout << " Tmax t31 : " << t31 << endl;
+  t32 = (planckr1/t31) - 273.15;
+  //cout << " Tmax t32 : " << t32 << endl;
+  t33 = log(-t32);
+  //cout << " Tmax t33 : " << t33 << endl;
 
   tmax = planckb / t33;
-  cout << " Tmax     : " << tmax << endl;
+  //cout << " Tmax     : " << tmax << endl;
+  return tmax;
+}
 
+
+void calcSmax()
+{
+  double planckr1, planckr2;
+  double planckb, planckf, plancko;
+  double rawmax;
+  double t41, t42, t43;
+
+  planckr1  = getPlanckR1();
+  planckr2  = getPlanckR2();
+  planckb   = getPlanckB();
+  planckf   = getPlanckF();
+  plancko   = getPlanckO();
+  rawmax    = calcRAWmax();
+
+  
   double t41 = planckr2 * (rawmax + plancko);
-  cout << " Smax t41 : " << t41 << endl;
+  //cout << " Smax t41 : " << t41 << endl;
   double t42 = ((planckr1/t41) + planckf);
-  cout << " Smax t42 : " << t42 << endl;
+  //cout << " Smax t42 : " << t42 << endl;
   double t43 = log(t42);
-  cout << " Smax t43 : " << t43 << endl;
+  //cout << " Smax t43 : " << t43 << endl;
   
   Smax = planckb / t43;
-  cout << " Smax    : " << Smax << endl;
+  //cout << " Smax    : " << Smax << endl;
+  return Smax;
+}
 
-  double t51 = planckr2 * (rawmin + plancko);
-  cout << " Smin t51 : " << t51 << endl;
-  double t52 = ((planckr1/t51) + planckf);
-  cout << " Smin t52 : " << t52 << endl;
-  double t53 = log(t52);
-  cout << " Smin t53 : " << t53 << endl;
+double calcSmin()
+{
+  double planckr1, planckr2;
+  double planckb, planckf, plancko;
+  double rawmin;
+  double t51, t52, t53;
+
+  planckr1  = getPlanckR1();
+  planckr2  = getPlanckR2();
+  planckb   = getPlanckB();
+  planckf   = getPlanckF();
+  plancko   = getPlanckO();
+  rawmin    = calcRAWmin();
+
+  t51 = planckr2 * (rawmin + plancko);
+  //cout << " Smin t51 : " << t51 << endl;
+  t52 = ((planckr1/t51) + planckf);
+  //cout << " Smin t52 : " << t52 << endl;
+  t53 = log(t52);
+  //cout << " Smin t53 : " << t53 << endl;
   
   Smin = planckb / t53;
-  cout << " Smin    : " << Smin << endl;
+  //cout << " Smin    : " << Smin << endl;
+  return Smin;
+}
 
+double calcSdelta()
+{
+  double Smax, Smin, Sdelta;
+  Smax   = calcSmax();
+  Smin   = calcSmin();
   Sdelta = Smax - Smin;
-  cout << " Sdelta  : " << Sdelta << endl;
+  //cout << " Sdelta  : " << Sdelta << endl;
+  return Sdelta;
 }
 
 
